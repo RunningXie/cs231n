fe64150fa6d8aa8d74854f50432efd0d1581219c
xieyichen <xieyichen124@163.com>
2018-04-26 20:08:46 +0800

    features finished

--- assignment1/cs231n/features.py
+++ assignment1/cs231n/features.py
@@ -32,7 +32,7 @@ def extract_features(imgs, feature_fns, verbose=False):
   feature_dims = []
   first_image_features = []
   for feature_fn in feature_fns:
-    feats = feature_fn(imgs[0].squeeze())
+    feats = feature_fn(imgs[0].squeeze())#squeeze  除去size为1的维度,img[0].shape=(32*32*3)
     assert len(feats.shape) == 1, 'Feature functions must be one-dimensional'
     feature_dims.append(feats.size)
     first_image_features.append(feats)
@@ -41,12 +41,12 @@ def extract_features(imgs, feature_fns, verbose=False):
   # big array to store all features as columns.
   total_feature_dim = sum(feature_dims)
   imgs_features = np.zeros((num_images, total_feature_dim))
-  imgs_features[0] = np.hstack(first_image_features).T
+  imgs_features[0] = np.hstack(first_image_features).T#hsatck()水平拼接
 
   # Extract features for the rest of the images.
   for i in xrange(1, num_images):
     idx = 0
-    for feature_fn, feature_dim in zip(feature_fns, feature_dims):
+    for feature_fn, feature_dim in zip(feature_fns, feature_dims):#zip() 函数用于将可迭代的对象作为参数，将对象中对应的元素打包成一个个元组
       next_idx = idx + feature_dim
       imgs_features[i, idx:next_idx] = feature_fn(imgs[i].squeeze())
       idx = next_idx
@@ -64,34 +64,34 @@ def rgb2gray(rgb):
 
     Returns:
       gray : grayscale image
-  
+
   """
   return np.dot(rgb[...,:3], [0.299, 0.587, 0.144])
 
 
 def hog_feature(im):
   """Compute Histogram of Gradient (HOG) feature for an image
-  
+
        Modified from skimage.feature.hog
        http://pydoc.net/Python/scikits-image/0.4.2/skimage.feature.hog
-     
+
      Reference:
        Histograms of Oriented Gradients for Human Detection
        Navneet Dalal and Bill Triggs, CVPR 2005
-     
+
     Parameters:
       im : an input grayscale or rgb image
-      
+
     Returns:
       feat: Histogram of Gradient (HOG) feature
-    
+
   """
-  
+
   # convert rgb to grayscale if needed
   if im.ndim == 3:
     image = rgb2gray(im)
   else:
-    image = np.at_least_2d(im)
+    image = np.at_least_2d(im)#这一方法可以保证所有的输入X至少是二维数组，如果是一维数组则会转化为一个二位的1*len(X)的数组
 
   sx, sy = image.shape # image size
   orientations = 9 # number of gradient bins
@@ -104,7 +104,7 @@ def hog_feature(im):
   grad_mag = np.sqrt(gx ** 2 + gy ** 2) # gradient magnitude
   grad_ori = np.arctan2(gy, (gx + 1e-15)) * (180 / np.pi) + 90 # gradient orientation
 
-  n_cellsx = int(np.floor(sx / cx))  # number of cells in x
+  n_cellsx = int(np.floor(sx / cx))  # number of cells in x,floor()向下取整
   n_cellsy = int(np.floor(sy / cy))  # number of cells in y
   # compute orientations integral images
   orientation_histogram = np.zeros((n_cellsx, n_cellsy, orientations))
@@ -112,15 +112,15 @@ def hog_feature(im):
     # create new integral image for this orientation
     # isolate orientations in this range
     temp_ori = np.where(grad_ori < 180 / orientations * (i + 1),
-                        grad_ori, 0)
+                        grad_ori, 0)#当conditon的某个位置的为true时，输出x的对应位置的元素，否则选择y对应位置的元素
     temp_ori = np.where(grad_ori >= 180 / orientations * i,
                         temp_ori, 0)
     # select magnitudes for those orientations
     cond2 = temp_ori > 0
     temp_mag = np.where(cond2, grad_mag, 0)
-    orientation_histogram[:,:,i] = uniform_filter(temp_mag, size=(cx, cy))[cx/2::cx, cy/2::cy].T
-  
-  return orientation_histogram.ravel()
+    orientation_histogram[:, :, i] = uniform_filter(temp_mag, size=(cx, cy))[cx // 2::cx, cy // 2::cy].T#//也表示除法，/进行除法运算结果为float型，报错
+
+  return orientation_histogram.ravel()#将多维数组降为一维
 
 
 def color_histogram_hsv(im, nbin=10, xmin=0, xmax=255, normalized=True):
@@ -139,10 +139,10 @@ def color_histogram_hsv(im, nbin=10, xmin=0, xmax=255, normalized=True):
     input image.
   """
   ndim = im.ndim
-  bins = np.linspace(xmin, xmax, nbin+1)
+  bins = np.linspace(xmin, xmax, nbin+1)#生成一个从xmin到xmax共有nbin+1个元素的矩阵
   hsv = matplotlib.colors.rgb_to_hsv(im/xmax) * xmax
-  imhist, bin_edges = np.histogram(hsv[:,:,0], bins=bins, density=normalized)
-  imhist = imhist * np.diff(bin_edges)
+  imhist, bin_edges = np.histogram(hsv[:,:,0], bins=bins, density=normalized)#对一维数组进行直方图统计，bins指定统计的区间个数，即对统计范围的等分数
+  imhist = imhist * np.diff(bin_edges)#diff()计算后一个值减去前一个值，从而获得两个hist的间距
 
   # return histogram
   return imhist
